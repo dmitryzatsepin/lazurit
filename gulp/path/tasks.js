c7bba0561457ad70e72224a10498fd88ab4e9ca6
Dmitry Zatsepin <wdm.dimpin@gmail.com>
2019-11-13 16:59:11 +0500

    Initial commit

---
+++ gulp/path/tasks.js
@@ -0,0 +1,12 @@
+module.exports = [
+	'./gulp/tasks/clean',
+	'./gulp/tasks/pug',
+	'./gulp/tasks/styles',
+	'./gulp/tasks/grid',
+	'./gulp/tasks/fonts',
+	'./gulp/tasks/scripts',
+	'./gulp/tasks/images',
+	'./gulp/tasks/svg',
+	'./gulp/tasks/watch',
+	'./gulp/tasks/server'
+];
