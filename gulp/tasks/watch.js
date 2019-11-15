2fdb9c7544791b0978fd665e2f8efb1f2c40edf8
Dmitry Zatsepin <wdm.dimpin@gmail.com>
2019-11-15 08:43:23 +0500

    gel losk before start

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,11 +4,13 @@ module.exports = function() {
 		$.gulp.series('pug')),
 		$.gulp.watch('src/assets/styles/**/*.sass',
 		$.gulp.series('styles:dev')),
+		$.gulp.watch('./smartgrid.js',
+		$.gulp.series('grid')),
 		$.gulp.watch('src/assets/img/**/*.{png,jpg,gif}',
 		$.gulp.series('img:dev')),
 		$.gulp.watch('src/assets/img/svg/*.svg',
 		$.gulp.series('svg')),
-		$.gulp.watch('src/assets/js/**/*.js', 
+		$.gulp.watch('src/assets/js/**/*.js',
 		$.gulp.series('js:dev'))
 	});
 };
